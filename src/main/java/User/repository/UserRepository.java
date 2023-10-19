40de4cc17bbd1f5571813c9ff49a48d50dadfb63
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-10-19 18:54:13 +0530

    minor changes to userRepository

--- src/main/java/User/repository/UserRepository.java
+++ src/main/java/User/repository/UserRepository.java
@@ -4,16 +4,16 @@ package User.repository;
 import User.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 
+@EnableJpaRepositories
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
     boolean existsByUsername(String username);
 
-    @Query("SELECT u FROM User u WHERE u.username = :username AND u.password = :password")
     User findByUsernameAndPassword(String username, String password);
-
 
     User findByUsername(String username);
 }
